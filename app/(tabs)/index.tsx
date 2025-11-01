1e07a89680709b844180ae17af8ddb88fe476b86
DouglasEmmanuel <emmanueldouglas2121@gmail.com>
2025-10-31 19:49:20 -0700

    -feat : added convex-functions with schemas

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,21 @@
 import { Image } from 'expo-image';
 import { Platform, StyleSheet , View } from 'react-native';
-import { Link } from 'expo-router';
 import { useColorMode } from '@/theme/colourModeContext';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import {useMutation , useQuery} from 'convex/react' ;
+import {api} from '../../convex/_generated/api' ;
 export default function HomeScreen() {
-   const { themeMode, toggleTheme, colors } = useColorMode()
-    const isDarkMode = themeMode === 'dark'
+   const { themeMode, toggleTheme, colors } = useColorMode();
+    const isDarkMode = themeMode === 'dark';
+        const createTodo = useMutation(api.todos.createTodo) ;
+        const allTodo = useQuery(api.todos.getAllTodos);
+        const activeTodo = useQuery(api.todos.getActiveTodos);
+        const completedTodo = useQuery(api.todos.getCompletedTodos) ;
+        const deleteAllTodo = useMutation(api.todos.deleteAllTodos);
+        const deleteSingleTodo = useMutation(api.todos.deleteSingleTodo);
+        const updateSingleTodo = useMutation(api.todos.updateTodo);
   return (
+ 
       <SafeAreaView style={{
         backgroundColor:colors.background
       }}>
---
+++ convex/todos.ts
@@ -0,0 +1,86 @@
+import {mutation,query} from './_generated/server' ;
+import {v} from 'convex/values' ;
+
+export const createTodo = mutation({
+args:{
+    title:v.string() ,
+    completed :v.boolean() ,
+
+},
+handler:async (ctx, args) => {
+    await ctx.db.insert("todo",{
+        title:args.title,
+        completed:args.completed,
+    });
+},
+
+});
+
+
+export const updateTodo = mutation({
+    args:{
+    id:v.id("todo") ,
+    completed :v.boolean()  ,
+    },
+handler:async(ctx , args)=>{
+    await ctx.db.patch(args.id,{
+        completed:args.completed,
+    });
+},
+});
+
+
+
+
+export const getAllTodos = query({
+    args :{},
+    handler : async (ctx , args) =>{
+     return await ctx.db.query("todo").collect();
+    }
+})
+
+
+
+
+export const getActiveTodos = query({
+  args: {},
+  handler: async (ctx, args) => {
+    return await ctx.db
+      .query("todo")
+      .filter((q) => q.eq(q.field("completed"), false))
+      .collect();
+  },
+});
+
+
+export const getCompletedTodos = query({
+  args: {},
+  handler: async (ctx, args) => {
+    return await ctx.db
+      .query("todo")
+      .filter((q) => q.eq(q.field("completed"), true))
+      .collect();
+  },
+});
+
+
+export const deleteAllTodos = mutation({
+    args:{},
+    handler:async (ctx, args)=> {
+        const todo = await ctx.db.query('todo').collect();
+
+        await Promise.all(todo.map((todo)=> ctx.db.delete(todo._id)));
+    },
+});
+
+
+export const deleteSingleTodo = mutation({
+    args:{
+        id:v.id('todo'),
+    },
+
+    handler:async(ctx, args) =>{
+        await ctx.db.delete(args.id);
+        
+    },
+})
